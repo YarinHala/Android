21475f2c325e5f3ed7372f0e91dd82e9d92776ee
blicount <licountb5@gmail.com>
2018-08-05 10:36:21 +0200

    config chenge

--- MasterMindSafeEdition/app/src/main/java/yarinhala/com/shenkar/mastermindsafeedition/MainActivity.java
+++ MasterMindSafeEdition/app/src/main/java/yarinhala/com/shenkar/mastermindsafeedition/MainActivity.java
@@ -1,6 +1,8 @@
 package yarinhala.com.shenkar.mastermindsafeedition;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,51 +16,73 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        final MediaPlayer beckgroundsong;
+        final MediaPlayer buttonEffect;
+        AudioManager amanger;
+        int currentvol;
+
         Button levels_btn = findViewById(R.id.start_game);
         Button scores_btn = findViewById(R.id.scores_table);
         Button instructions_btn = findViewById(R.id.instructions);
         Button options_btn = findViewById(R.id.options);
         Button exit_btn = findViewById(R.id.exit);
 
+        beckgroundsong = MediaPlayer.create(this, R.raw.launchapp);
+        beckgroundsong.setLooping(true);
+        beckgroundsong.start();
+
+        buttonEffect = MediaPlayer.create(this, R.raw.buttoneffect);
+
+
         levels_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Levels.class));
+                beckgroundsong.pause();
+                buttonEffect.start();
+                beckgroundsong.start();
+                startActivity(new Intent(MainActivity.this, Levels.class));
             }
         });
 
         scores_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Scores.class));
+                beckgroundsong.pause();
+                buttonEffect.start();
+                beckgroundsong.start();
+                startActivity(new Intent(MainActivity.this, Scores.class));
             }
         });
 
         instructions_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Instructions.class));
+                beckgroundsong.pause();
+                buttonEffect.start();
+                beckgroundsong.start();
+                startActivity(new Intent(MainActivity.this, Instructions.class));
             }
         });
 
         options_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Options.class));
+                beckgroundsong.pause();
+                buttonEffect.start();
+                beckgroundsong.start();
+                startActivity(new Intent(MainActivity.this, Options.class));
             }
         });
 
         exit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                beckgroundsong.pause();
+                buttonEffect.start();
                 finish();
                 System.exit(0);
             }
         });
     }
-
-
-
-
 
 }
